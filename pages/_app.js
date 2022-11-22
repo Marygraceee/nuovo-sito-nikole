050bf393412f682db52a2bcb2696135475db29a5
Marygraceee <ma.grasso1999@gmail.com>
2022-11-22 23:18:03 +0100

    blog alla bene e meglio

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,7 @@
 import '../styles/globals.css'
-import axios from 'axios';
 import Topnav from '../components/Topnav';
 import Head from 'next/head';
 import Footer from '../components/Footer';
-
-
-
-axios.get('http://localhost:1337/api/articles').then(response => {
-  console.log(response.data);
-});
-
 
  
 
--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import axios from 'axios';
+import Blogcard from '../components/Blogcard';
 
-const blog = () => {
+
+export async function getStaticProps(context) {
+  const req = await axios.get(`${process.env.PAYLOAD_PUBLIC_BASE_DNS}/api/posts`);
+  const articles = req.data.docs
+  
+  return {
+    props: {
+ articles
+    }, // will be passed to the page component as props
+  }
+}
+ 
+
+const blog = ({articles}) => {
   return (
-    <div>blog</div>
+    
+    <section className="min-h-screen w-full bg-sfondi flex lg:flex-row flex-col justify-center items-center">
+      <div className="bg-red-400 w-full">
+        <h2>Categorie:</h2>
+      </div>
+      <div className="grid grid-cols-2 w-full gap-10 p-10">
+     {articles.map((article)=>(
+      <Blogcard key={article.id} article={article} /> 
+     ))}
+      </div>
+    
+    </section>
   )
 }
 
